0cdecd946dd6a7b7e3b15b02f3f3d6365c1b8128
Vyacheslav Voronenko <git@voronenko.info>
2021-02-04 12:07:04 +0100

    improve(example) added provisioning example over aws

---
+++ box-example/providers/staging/instance.tf
@@ -0,0 +1,111 @@
+resource "aws_instance" "swarm_master" {
+  ami                         = local.app_ami
+  availability_zone           = local.app_availability_zone_1
+  ebs_optimized               = false
+  instance_type               = "t3.small"
+  monitoring                  = false
+  key_name                    = local.app_instance_defaultkey
+  subnet_id                   = data.aws_subnet.pub_subnet1.id
+  vpc_security_group_ids      = [aws_security_group.swarm_demo.id, data.aws_security_group.sg-bastion.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 60
+    delete_on_termination = true
+  }
+
+  tags = {
+    Environment = local.env
+    Name        = "swarm_master"
+    Role        = "swarm_master"
+    Project     = local.project
+  }
+}
+
+resource "aws_instance" "swarm_worker" {
+  ami                         = local.app_ami
+  availability_zone           = local.app_availability_zone_1
+  ebs_optimized               = false
+  instance_type               = "t3.small"
+  monitoring                  = false
+  key_name                    = local.app_instance_defaultkey
+  subnet_id                   = data.aws_subnet.pub_subnet1.id
+  vpc_security_group_ids      = [aws_security_group.swarm_demo.id, data.aws_security_group.sg-bastion.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 60
+    delete_on_termination = true
+  }
+
+  tags = {
+    Environment = local.env
+    Name        = "swarm_worker"
+    Role        = "swarm_worker"
+    Project     = local.project
+  }
+}
+
+
+resource "aws_security_group" "swarm_demo" {
+
+  name        = "${local.project}-${local.env}-app"
+  vpc_id      = data.aws_vpc.app_vpc.id
+  description = "${local.project} ${local.env} appservers (monitoring, http, https, ssh)"
+
+
+  # cross communication
+  ingress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      self = true
+    }
+
+
+  # Internal HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Internal HTTPS access from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #ssh from anywhere (unnecessary)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # ping access from anywhere
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = local.env
+    Project     = local.project
+  }
+}
---
+++ box-example/providers/staging/variables-dictionaries.tf
@@ -0,0 +1,99 @@
+variable "workspace_to_environment_map" {
+  type = map(string)
+  default = {
+    default = "staging"
+    dev     = "dev"
+    qa      = "qa"
+    staging = "staging"
+    prod    = "prod"
+  }
+}
+
+variable "environment_to_instance_size_map" {
+  type = map(string)
+  default = {
+    default = "t3.small"
+    dev     = "t3.small"
+    qa      = "t3.small"
+    staging = "t3.small"
+    prod    = "t3.medium"
+  }
+}
+
+variable "environment_to_instance_disk_map" {
+  type = map(string)
+  default = {
+    default = "30"
+    dev     = "30"
+    qa      = "35"
+    staging = "30"
+    prod    = "35"
+  }
+}
+
+variable "environment_to_fixedip_map" {
+  type = map(string)
+  default = {
+    default = "false"
+    dev     = "false"
+    qa      = "false"
+    staging = "false"
+    prod    = "false"
+  }
+}
+
+variable "environment_to_region_map" {
+  type = map(any)
+  default = {
+    default = "eu-west-1"
+    dev     = "eu-west-1"
+    qa      = "eu-west-1"
+    staging = "eu-west-1"
+    prod    = "eu-west-1"
+  }
+}
+
+variable "environment_to_keys_map" {
+  type = map(any)
+  default = {
+    default = "voronenko_info"
+    dev     = "voronenko_info"
+    qa      = "voronenko_info"
+    staging = "voronenko_info"
+    prod    = "voronenko_info"
+  }
+}
+
+variable "environment_to_instance_profile_map" {
+  type = map(any)
+  default = {
+    default = "web_instance_profile_development"
+    dev     = "web_instance_profile_development"
+    qa      = "web_instance_profile_development"
+    staging = "web_instance_profile_development"
+    prod    = "web_instance_profile_production"
+  }
+}
+
+
+variable "environment_to_availability_zone_1" {
+  type = map(any)
+  default = {
+    default = "eu-west-1a"
+    dev     = "eu-west-1a"
+    qa      = "eu-west-1a"
+    staging = "eu-west-1a"
+    prod    = "eu-west-1a"
+  }
+}
+
+variable "environment_to_availability_zone_2" {
+  type = map(any)
+  default = {
+    default = "eu-west-1b"
+    dev     = "eu-west-1b"
+    qa      = "eu-west-1b"
+    staging = "eu-west-1b"
+    prod    = "eu-west-1c"
+  }
+}
